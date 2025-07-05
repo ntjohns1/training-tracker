133b639b33d566b7c6898c512005dbc73acad003
ntjohns1 <nelsontjohns@gmail.com>
2025-07-05 08:34:20 -0400

    Initital commit

---
+++ src/main/java/com/noslen/training_tracker/TrainingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.noslen.training_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noslen/training_tracker/TrainingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noslen.training_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
