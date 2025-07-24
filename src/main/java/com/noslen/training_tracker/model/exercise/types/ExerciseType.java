5b4cf2b04871058220d21e1efecb40f799258423
ntjohns1 <nelsontjohns@gmail.com>
2025-07-23 21:28:16 -0400

    implement CustomSerializableEnum for exercise and progression types

--- src/main/java/com/noslen/training_tracker/model/exercise/types/ExerciseType.java
+++ src/main/java/com/noslen/training_tracker/model/exercise/types/ExerciseType.java
@@ -1,12 +1,24 @@
 package com.noslen.training_tracker.model.exercise.types;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.noslen.training_tracker.util.CustomSerializableEnum;
 
-public enum ExerciseType {
-    BARBELL, BODYWEIGHT, BODYWEIGHT_LOADABLE, CABLE, DUMBBELL, FREEMOTION, MACHINE, MACHINE_ASSISTANCE, SMITH_MACHINE;
+public enum ExerciseType implements CustomSerializableEnum {
+    BARBELL, BODYWEIGHT_ONLY, BODYWEIGHT_LOADABLE, CABLE, DUMBBELL, FREEMOTION, MACHINE, MACHINE_ASSISTANCE, SMITH_MACHINE;
 
     @JsonValue
     public String getValue() {
-        return this.name().toLowerCase();
+        return switch (this) {
+            case BODYWEIGHT_ONLY -> "bodyweight-only";
+            case BODYWEIGHT_LOADABLE -> "bodyweight-loadable";
+            case MACHINE_ASSISTANCE -> "machine-assistance";
+            case SMITH_MACHINE -> "smith-machine";
+            default -> this.name().toLowerCase();
+        };
+    }
+
+    @Override
+    public String getSerializedValue() {
+        return getValue();
     }
 }
